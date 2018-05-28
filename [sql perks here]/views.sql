ef986f6aec3daabda1e87df97e37e494f25168d5
olivergabriel3 <olivergab19@gmail.com>
2018-05-28 16:32:18 +0800

    views

--- [sql perks here]/views.sql
+++ [sql perks here]/views.sql
@@ -4,12 +4,24 @@
 -- ----------------------------
 create view student_profiling as
 select
-	Stud_NO as STUD_NO,
-    concat(Stud_FNAME,' ',Stud_LNAME) as FULLNAME,
-    concat(Stud_COURSE,' ',Stud_YEAR_LEVEL,'-',Stud_YEAR_LEVEL) as COURSE,
-    Stud_STATUS as STUD_STATUS
-from r_stud_profile
-where Stud_STATUS = 'Regular' or Stud_STATUS = 'Irregular';
+    SP.Stud_NO as STUD_NO,
+    if(SP.Stud_MNAME is null,concat(SP.Stud_FNAME,' ',SP.Stud_LNAME),concat(SP.Stud_FNAME,' ',SP.Stud_MNAME,' ',SP.Stud_LNAME)) as FULLNAME,
+    concat(SP.Stud_COURSE,' ',SP.Stud_YEAR_LEVEL,'-',SP.Stud_YEAR_LEVEL) as COURSE,
+    SB.Batch_YEAR as `BATCH YEAR`,
+    SP.Stud_GENDER as GENDER,
+    date_format(SP.Stud_BIRTH_DATE,'%M %d, %Y') as `BIRTH DATE`,
+    SP.Stud_BIRTH_PLACE as `BIRTH PLACE`,
+    SP.Stud_CITY_ADDRESS as `CITY ADDRESS`,
+    SP.Stud_PROVINCIAL_ADDRESS as `PROVINCIAL ADDRESS`,
+    SP.Stud_TELEPHONE_NO as `TELEPHONE NO`,
+    SP.Stud_MOBILE_NO as `MOBILE NO`,
+    SP.Stud_EMAIL as EMAIL,
+    SB.Stud_STATUS as STUD_STATUS
+from  r_stud_batch SB
+inner join r_stud_profile SP
+    on SP.Stud_NO = SB.Stud_NO
+where SB.Batch_YEAR = (select Batch_YEAR from r_batch_details order by Batch_ID desc limit 1)
+    and SB.Stud_STATUS = 'Regular' or SB.Stud_STATUS = 'Irregular';
 
 -- ----------------------------
 -- View structure for student_counseling
