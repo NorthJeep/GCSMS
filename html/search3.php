c98f13df00438316046372bca92608e8ddba23b7
Vexolin <38964212+Vexolin@users.noreply.github.com>
2018-05-31 15:45:12 +0800

    intellisense

--- html/search3.php
+++ html/search3.php
@@ -5,14 +5,14 @@
       $output = '';
       if($_POST["query"] != '')
       {
-        $query = "SELECT * FROM r_stud_profile WHERE STUD_NO LIKE '%".$_POST["query"]."%'";  
+        $query = "SELECT * FROM r_stud_profile WHERE Stud_NO LIKE '%".$_POST["query"]."%'";  
         $result = mysqli_query($db, $query);  
         $output = '<ul class="list-unstyled">';  
         if(mysqli_num_rows($result) > 0)  
         {  
              while($row = mysqli_fetch_array($result))  
              {  
-                  $output .= '<li class="search-item-name">'.$row["STUD_FNAME"].' '.$row["STUD_MNAME"].' '.$row["STUD_LNAME"].'<small class="hide student-no-hide">'.$row["STUD_NO"].'</small></li>';  
+                  $output .= '<li class="search-item-name">'.$row["Stud_FNAME"].' '.$row["Stud_MNAME"].' '.$row["Stud_LNAME"].'<small class="hide student-no-hide">'.$row["Stud_NO"].'</small></li>';  
              }  
         }  
         else  
--- html/search4.php
+++ html/search4.php
@@ -5,14 +5,14 @@
       $output = '';
       if($_POST["query"] != '')
       {
-        $query = "SELECT * FROM r_stud_profile WHERE STUD_NO LIKE '%".$_POST["query"]."%'";  
+        $query = "SELECT * FROM r_stud_profile WHERE Stud_NO LIKE '%".$_POST["query"]."%'";  
         $result = mysqli_query($db, $query);  
         $output = '<ul class="list-unstyled">';  
         if(mysqli_num_rows($result) > 0)  
         {  
              while($row = mysqli_fetch_array($result))  
              {  
-                  $output .= '<li class="search-item-no">'.$row["STUD_NO"].' <small class="hide student-name-hide">'.$row["STUD_FNAME"].' '.$row["STUD_MNAME"].' '.$row["STUD_LNAME"].'</small></li>';  
+                  $output .= '<li class="search-item-no">'.$row["Stud_NO"].' <small class="hide student-name-hide">'.$row["Stud_FNAME"].' '.$row["Stud_MNAME"].' '.$row["Stud_LNAME"].'</small></li>';  
              }  
         }  
         else  
