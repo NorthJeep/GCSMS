dd5e14eef24b92126d567da79427c286721d264f
John Carlo Gutierrez <jcgutierrez0102@gmail.com>
2018-06-01 14:20:30 +0800

    Fix exclusive content

--- [sql perks here]/DB fix for Carlo (carlo exclusive, don't open).sql
+++ [sql perks here]/DB fix for Carlo (carlo exclusive, don't open).sql
@@ -16,7 +16,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */; 
 /*!40101 SET NAMES utf8mb4 */; 
  
- CREATE DATABASE IF NOT EXISTS pupqcdb;
+ DROP DATABASE IF EXISTS pupqcdb;
+ CREATE DATABASE pupqcdb;
  USE pupqcdb;
 -- 
 -- Database: `pupqcdb` 
