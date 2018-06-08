33c9d0d3117e04ff0d9813bd712054b63643314c
Eyrafries <36601339+eyrafries@users.noreply.github.com>
2018-06-08 21:50:30 +0800

    Seryoso
    tinanggal ko lang mga dapat tanggalin
    at binaligtad

--- html/homeandfamily_background.php
+++ html/homeandfamily_background.php
@@ -6,9 +6,9 @@
 
 			if($family_parent == "Guardian")
 			{
-				/*$family_parent = $_POST['family_parent'];
+				$studno_ref = $_POST['studno_ref'];
 
-				$result  =  mysqli_query($db, "SELECT  *  FROM  r_stud_guardian  WHERE  Stud_NO='$family_parent'") or die (mysqli_error($db));
+				$result  =  mysqli_query($db, "SELECT  *  FROM  r_stud_guardian  WHERE  Stud_NO='$studno_ref'") or die (mysqli_error($db));
 				$num_rows  =  mysqli_num_rows($result);
  
 				if  ($num_rows)  
@@ -16,7 +16,7 @@
 					header("location:  profiling.php?remarks=failed");
 				}
 				else
-				{*/
+				{
 					$studno_ref = $_POST['studno_ref'];
 					$fam_fname =$_POST['fam_fname'];
 					$fam_mname =$_POST['fam_mname'];
@@ -32,9 +32,20 @@
 					mysqli_query($db, "INSERT  INTO  r_stud_guardian(Stud_NO, Guardian_FNAME, Guardian_MNAME, Guardian_LNAME, Guardian_AGE, Stud_GUARDIAN_RELATION, Guardian_EDUC_ATTAINMENT, Guardian_OCCUPATION, Guardian_EMPLOYER_NAME, Guardian_EMPLOYER_ADDRESS)
 						VALUES('$studno_ref', '$fam_fname', '$fam_mname', '$fam_lname', '$fam_age', '$fam_lifestats', '$fam_educattain', '$fam_occu', '$fam_employname', '$fam_employadd')");
 					header("location:  profiling.php?remarks=success");
-				//}
+				}
 			}
 			else{
+				$family_parent = $_POST['family_parent'];
+
+				$result  =  mysqli_query($db, "SELECT  *  FROM  r_stud_family_bg_details  WHERE  FamilyBG_INFO='$family_parent'") or die (mysqli_error($db));
+				$num_rows  =  mysqli_num_rows($result);
+ 
+				if  ($num_rows)  
+				{
+					header("location:  profiling.php?remarks=failed");
+				}
+				else
+				{
 					$studno_ref = $_POST['studno_ref'];
 					$family_parent = $_POST['family_parent'];
 					$fam_fname =$_POST['fam_fname'];
@@ -50,5 +61,6 @@
 					mysqli_query($db, "INSERT  INTO  r_stud_family_bg_details(Stud_NO_REFERENCE, FamilyBG_INFO, Info_FNAME, Info_MNAME, Info_LNAME, Info_AGE, Info_STAT, Info_EDUC_ATTAINMENT, Info_OCCUPATION, Info_EMPLOYER_NAME, Info_EMPLOYER_ADDRESS)
 						VALUES('$studno_ref', '$family_parent', '$fam_fname', '$fam_mname', '$fam_lname', '$fam_age', '$fam_lifestats', '$fam_educattain', '$fam_occu', '$fam_employname', '$fam_employadd')") or die (mysqli_error($db));
 					header("location:  profiling.php?remarks=success");
+				}
 			}
 ?>
